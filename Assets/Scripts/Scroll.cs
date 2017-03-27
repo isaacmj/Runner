2253702f2b36a16503e2c709620f0b06f5f25d92
Isaac Montenegro <isaacmj@yahoo.com>
2017-03-26 20:04:05 -0600

    Base Project

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	public float speed = 0.5f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0f);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset; 
+	}
+}
