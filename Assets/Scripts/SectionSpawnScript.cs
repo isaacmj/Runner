a6729b97feb90ccc2814b459de8199dd4c50555c
Isaac Montenegro <isaacmj@yahoo.com>
2017-03-26 21:03:44 -0600

    Changed to sppawn complete sections and use a signaler to trigger next spawn

---
+++ Assets/Scripts/SectionSpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionSpawnScript : MonoBehaviour {
+
+	public Transform pivot;
+	public GameObject[] obj;
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();		
+	}
+
+	void Spawn() {
+		Instantiate (obj [Random.Range (0, obj.Length)], pivot.position, Quaternion.identity);
+	}
+
+	public void SignalSpawn() {
+		Spawn ();
+	}
+}
---
+++ Assets/Scripts/SpawnSignalDetectScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSignalDetectScript : MonoBehaviour {
+	public GameObject spawner;
+	private SectionSpawnScript spawnScript;
+
+	void Start () {
+		spawnScript = spawner.GetComponent<SectionSpawnScript> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Signaler") {
+			spawnScript.SignalSpawn ();
+			print ("Singal");
+		}
+	}
+}
