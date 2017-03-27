55b1167045dda40ecbc60895157d41a7982c26cf
Isaac Montenegro <isaacmj@yahoo.com>
2017-03-26 21:38:11 -0600

    Some fixes and more sections

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class Destroyer : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
-			Application.LoadLevel (1);
-			return;
-		}
 		if (other.gameObject.transform.parent) {
 			Destroy (other.gameObject.transform.parent.gameObject);
 		} else {
