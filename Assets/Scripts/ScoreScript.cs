13c3760b1ae3b25eb66c379cfdc140a2790a2f01
Isaac Montenegro <isaacmj@yahoo.com>
2017-04-01 12:25:06 -0600

    Adding basic HUD

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+	public Text text;
+	private float playerScore = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		playerScore += Time.deltaTime;
+		text.text = "Score: " + getDisplayableScore ();
+	}
+
+	void OnDisable() {
+		PlayerPrefs.SetInt ("Score", getDisplayableScore());
+	}
+
+	private int getDisplayableScore() {
+		return (int)(playerScore * 100);
+	}
+}
