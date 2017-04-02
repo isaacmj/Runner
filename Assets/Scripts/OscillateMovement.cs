096da6afe61249685008daa83fc4102839f1110d
Isaac Montenegro <isaacmj@yahoo.com>
2017-04-02 16:36:12 -0600

    Added Spaceship

---
+++ Assets/Scripts/OscillateMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateMovement : MonoBehaviour {
+	private float startPositionY;
+	public float magnitude;
+	public float frequency;
+
+	// Use this for initialization
+	void Start () {
+		startPositionY = transform.position.y;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yPosition = startPositionY + Mathf.Sin (Time.time * frequency) * magnitude;
+		transform.position = new Vector3 (transform.position.x, yPosition, transform.position.z);
+	}
+}
